df81fefb8522e29a42af5d295b90c613f8f45203
KevinTantaruna <kevindev@microsoft.com>
2018-03-16 09:08:04 -0500

    mi primer commit ymi background de p es azul

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Mi pagina para Git</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<h1 class="h1">Soy una pagina web para Git</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, magni facere. Consectetur veniam, voluptatibus saepe sint nemo quam ea aut odit earum ipsum ipsam, dolorem, omnis aliquid neque magnam vero!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem magni iusto aliquid quas nihil voluptate quibusdam. Asperiores, libero, ea! Tenetur laboriosam quis, natus at dolores et ducimus ab laudantium tempore!</p>
+		<hr>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, magni facere. Consectetur veniam, voluptatibus saepe sint nemo quam ea aut odit earum ipsum ipsam, dolorem, omnis aliquid neque magnam vero!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem magni iusto aliquid quas nihil voluptate quibusdam. Asperiores, libero, ea! Tenetur laboriosam quis, natus at dolores et ducimus ab laudantium tempore!</p>
+		<footer>Kevin</footer>
+	</body>
+</html>
